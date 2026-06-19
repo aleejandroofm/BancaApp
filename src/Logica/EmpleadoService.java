0c752636588ffd25e47114fd29f8c740e1205a9a
aleejandroofm <ferrandizmartineza9@gmail.com>
2026-06-19 13:34:30 +0200

    Mejora de comentario Javadoc

--- src/Logica/EmpleadoService.java
+++ src/Logica/EmpleadoService.java
@@ -15,7 +15,12 @@ public class EmpleadoService {
     private LogAuditoriaDAO logAuditoriaDao = new LogAuditoriaDAO();
 
     /**
-     * Registra un cliente y guarda la traza usando tus campos exactos.
+     * Registra un nuevo cliente en el sistema y guarda de forma obligatoria la traza de auditoría,
+     * reflejando si la operación se completó con éxito o falló.
+     * La auditoría se ejecuta siempre en el bloque finally para garantizar el registro de la acción administrativa.
+     * @param nuevoUsuario objeto que contiene toda la información del cliente que se va a dar de alta.
+     * @param dniEmpleado DNI del empleado que realiza y autoriza la gestión desde su terminal.
+     * @throws PersistenciaException si ocurre un fallo de infraestructura o restricción de integridad en la base de datos.
      */
     public void registrarNuevoCliente(Usuario nuevoUsuario, String dniEmpleado) throws PersistenciaException {
         boolean resultadoExitoso = false;
@@ -31,7 +36,13 @@ public class EmpleadoService {
     }
 
     /**
-     * Modifica el estado de un IBAN y audita el resultado.
+     * Modifica el estado operativo de una cuenta bancaria (activación o bloqueo de fondos) y
+     * genera la correspondiente traza de auditoría con el resultado de la acción.
+     * @param numeroCuenta código IBAN de la cuenta corriente que se desea modificar.
+     * @param nuevoEstado boolean que determina el nuevo estado (true para HABILITAR_CUENTA, false para BLOQUEAR_CUENTA).
+     * @param dniEmpleado DNI del empleado responsable que ejecuta el cambio de estado en ventanilla.
+     * @throws CuentaNoEncontradaException si el IBAN facilitado no corresponde a ninguna cuenta registrada.
+     * @throws PersistenciaException si se produce un error crítico de conexión o escritura en el servidor relacional.
      */
     public void cambiarEstadoCuenta(String numeroCuenta, boolean nuevoEstado, String dniEmpleado) throws CuentaNoEncontradaException, PersistenciaException {
         
